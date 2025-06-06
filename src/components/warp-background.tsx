56b1784e251f57a13216fefc69057404118fe2fb
Alessandro <a@a.a>
2025-06-06 14:44:13 +0200

    commit

--- src/components/warp-background.tsx
+++ src/components/warp-background.tsx
@@ -1,21 +1,40 @@
 import { Warp, type WarpProps } from "@paper-design/shaders-react";
 
 interface WarpBackgroundProps extends WarpProps {
-  image?: string;
+  imageUrl?: string;
 }
 
-export default function WarpBackground(props: WarpProps) {
-  const defaultProps = {
-    speed: 0.4,
-    rotation: 0.5,
-    style: { width: "100%", height: "100%" },
+export default function WarpBackground(props: WarpBackgroundProps) {
+  const { imageUrl, ...rest } = props;
+
+  const warpProps = {
+    // speed: 0.4,
+    // rotation: 0.5,
+    style: { width: "100%", height: "100%", position: "relative" },
+    ...rest,
   };
 
   return (
     <Warp
-      {...defaultProps}
-      {...props}
-      style={{ ...defaultProps.style, ...props.style }}
-    />
+      {...warpProps}
+      style={{ ...warpProps.style, ...props.style } as React.CSSProperties}
+    >
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt="Background"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            pointerEvents: "none",
+            opacity: 0.85,
+          }}
+        />
+      )}
+    </Warp>
   );
 }
