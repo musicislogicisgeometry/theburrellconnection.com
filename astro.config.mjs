685cbf375f389be27b8f536942772dc7934ff617
Alessandro <a@a.a>
2025-06-13 15:06:37 +0200

    some progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,48 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-   output: 'server',
+  output: "server",
+  site: process.env.CI
+    ? "https://theburrellconnection.com"
+    : "http://localhost:4321",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   integrations: [react()],
 
   experimental: {
-    fonts: [{
-      provider: fontProviders.google(),
-      name: "Geist",
-      cssVariable: "--font-geist",
-      fallbacks: ["Inter", "sans-serif"],
-    }]
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Geist",
+        cssVariable: "--font-geist",
+        fallbacks: ["Inter", "sans-serif"],
+      },
+    ],
   },
 
-  adapter: vercel()
+  adapter: vercel(),
 });
+
+// import { defineConfig } from 'astro/config'
+// import react from '@astrojs/react'
+// import tailwind from '@astrojs/tailwind'
+
+// // https://astro.build/config
+// export default defineConfig({
+
+//   integrations: [
+//     react(),
+//     tailwind({
+//       applyBaseStyles: false,
+//     }),
+//   ],
+// })
